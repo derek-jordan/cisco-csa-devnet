5ecd8cfa8bdb110f6f3e79d117a2866a00c59c1a
Derek Jordan <djordan@derekjordan.com>
2020-04-09 15:23:29 -0500

    Commit sample test

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-print("Hello")
-print("Hello Again")
-print("Hello")
+def doubler(number):
+    result = number * 2
+    return result
+get_number = float(input("Type a Number: "))
+answer = doubler(get_number)
+print(answer)
